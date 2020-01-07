45c2ff040579362ea0cf614fcaeaf955a3efe23b
Alexander <hello@alexander.works>
2020-01-08 01:14:59 +0200

    Added: Cart page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../Context';
 
 function Header() {
+  const { cartItems } = useContext(Context);
+
+  function cartButton() {
+    console.log(cartItems);
+
+    if (cartItems.length > 0) {
+      return (
+        <button class='btn-cart'>
+          <span className='badge'>{cartItems.length}</span>
+          <i className='icon ri-shopping-cart-fill ri-fw ri-2x'></i>
+        </button>
+      );
+    } else {
+      return (
+        <button class='btn-cart'>
+          <i className='icon ri-shopping-cart-line ri-fw ri-2x'></i>
+        </button>
+      );
+    }
+  }
+  console.log(cartItems);
+
   return (
     <header>
       <div className='page-wrap'>
@@ -10,9 +33,7 @@ function Header() {
             <strong>Snap</strong>Photo
           </h2>
         </Link>
-        <Link to='/cart'>
-          <i className='icon ri-shopping-cart-line ri-fw ri-2x'></i>
-        </Link>
+        <Link to='/cart'>{cartButton()}</Link>
       </div>
     </header>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Context } from '../Context';
 
 function Cart() {
+  const { cartItems } = useContext(Context);
+  const itemsArr = cartItems.map(item => (
+    <li key={item.id}>
+      <img src={item.url} alt='' />
+      <h4>{item.id}</h4>
+    </li>
+  ));
   return (
     <main className='cart-page'>
       <h1>Check out</h1>
+
+      {cartItems.length > 0 ? (
+        <ul className='items-list'>{itemsArr}</ul>
+      ) : (
+        <div className='align-center'>
+          <h2>Oh, there is no items yet. </h2>
+          <p>
+            Please <Link to='/'>go to the gallery</Link> and select something
+            you like{' '}
+            <span role='img' aria-label='Smile'>
+              😊
+            </span>
+          </p>
+        </div>
+      )}
     </main>
   );
 }
